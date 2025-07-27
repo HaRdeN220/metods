48f5b7248e4cd6bbb168167ffaeb3086b02d1521
egor <vlad.filin_2000@mail.ru>
2025-07-27 13:01:30 +0300

    commit complete

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,27 @@ import java.util.Locale;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void checkLeapYear(int year){
-        if ((year%4==0 && year%100!=0)|| ( year %400==0)){
+    public static void checkLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
         }
     }
-    public static void checkVersionDevice(int clientOS, int clientDeviceYear){
-        int currentYear= LocalDate.now().getYear();
-        if (clientOS==0 && clientDeviceYear<currentYear){
+
+    public static void checkVersionDevice(int clientOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientOS == 0 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS== 1 && clientDeviceYear < currentYear) {
+        } else if (clientOS == 1 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
+
     public static String deliveryDayInformation(int deliveryDistance) {
         int totalDays = 1;
         if (deliveryDistance > 100) {
@@ -31,16 +33,17 @@ public class Main {
             totalDays += 2;
         } else if (deliveryDistance > 20) {
             totalDays += 1;
-        } else if (deliveryDistance<0){
+        } else if (deliveryDistance < 0) {
             return "Вы некорректно ввели дистанцию до своего дома";
         }
         return "Потребуется дней: " + totalDays;
     }
+
     public static void main(String[] args) {
         System.out.println("Задача №1");
         checkLeapYear(2020);
         System.out.println("Задача №2");
-        checkVersionDevice(1,2020);
+        checkVersionDevice(1, 2020);
         System.out.println("Задача №3");
         System.out.println(deliveryDayInformation(111));
     }
